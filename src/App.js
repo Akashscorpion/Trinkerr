1aaaf757e91f67f88f56b641cb1f7be7358a4179
Akashscorpion <gayaakash@gmail.com>
2021-07-05 06:28:27 +0530

    Signup and login-UI part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react'
+import Panel from './component/singuppanel'
+import Login from './component/loginpanel'
 import './App.css';
 
 function App() {
+  const [option, setOption] = useState(0);
+  const [userInfo, setuserInfo] = useState([]);
+  const [err, seterr] = useState("");
+
+useEffect(() => {
+  var queriesFromStorage = [JSON.parse(localStorage.getItem("1234567889"))];
+  setuserInfo(queriesFromStorage)
+}, [])
+
+  const optionFn=()=>{
+    setOption(1)
+  }
+  const optionFn1=()=>{
+    setOption(0)
+  }
+
+  const reg=(useData)=>{
+    if(useData.status=="User created")
+    setTimeout(() => {
+      optionFn1()
+    }, 5000);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className=''>
+			<header>
+				<div className={'header-headings ' + (option === 1 ? 'sign-in' : (option === 2 ? 'sign-up' : 'forgot')) }>
+				
+        <span className={option==1 && 'active'} onClick={optionFn}>Create an account</span>
+        	<span className={option ==0 &&  'active'} onClick={optionFn1}>Sign in to your account</span>
+				
+				</div>
+			</header>
+      <div id='panel'>
+
+      {option ==1?(<Panel reg={reg} />):(<Login />)}
+      </div>
+		    
+
+		</div>
     </div>
   );
 }
---
+++ src/component/loginpanel.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function login() {
+    return (
+        <div>
+           <div className='container'>
+		   <div>
+
+
+		  
+		  <form >
+		<div className='signin'>
+			<input id='login' name='login' type='tel' placeholder='Enter your mobile number' required /> <br/>
+		</div>
+		<input className='btn-submit-form' type='submit'  value='Login'/>
+			
+	</form>
+
+	</div>
+			
+		</div> 
+        </div>
+	)
+}
+
+export default login
